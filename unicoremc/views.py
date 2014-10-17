c7f065fcf13fca8ecf9d718204da9a38058cc970
Milton Madanda <madandat@gmail.com>
2014-10-17 10:50:16 +0200

    also list nginx configs

--- unicoremc/views.py
+++ unicoremc/views.py
@@ -13,6 +13,14 @@ def list_supervisor_configs_view(request, *args, **kwargs):
     return render(request, 'admin/unicoremc/supervisor.html', context)
 
 
+@admin.site.register_view('nginx/', 'Nginx Configs')
+def list_nginx_configs_view(request, *args, **kwargs):
+    context = {
+        'nginx_configs': NodeManager().nginx_configs,
+    }
+    return render(request, 'admin/unicoremc/nginx.html', context)
+
+
 @admin.site.register_view('supervisor/config/', 'Supervisor Configs Detail')
 def supervisor_config_view(request, *args, **kwargs):
     filename = request.GET.get('filename')
@@ -24,3 +32,16 @@ def supervisor_config_view(request, *args, **kwargs):
         raise Http404
 
     return render(request, 'admin/unicoremc/supervisor_detail.html', context)
+
+
+@admin.site.register_view('nginx/config/', 'Nginx Configs Detail')
+def nginx_config_view(request, *args, **kwargs):
+    filename = request.GET.get('filename')
+    if filename:
+        context = {
+            'config': NodeManager().find_nginx(filename),
+        }
+    else:
+        raise Http404
+
+    return render(request, 'admin/unicoremc/nginx_detail.html', context)
