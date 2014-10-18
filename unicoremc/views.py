dba068dfb1ff3a11984ab6d2f64502cfb4a858a4
Milton Madanda <madandat@gmail.com>
2014-10-18 12:51:02 +0200

    add login required

--- unicoremc/views.py
+++ unicoremc/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import Http404
+from django.contrib.auth.decorators import login_required
 
 from unicoremc.utils import NodeManager
 from unicoremc import constants
@@ -9,6 +10,7 @@ def home(request):
     return render(request, 'unicoremc/home.html', {})
 
 
+@login_required(login_url='/')
 def list_supervisor_configs_view(request, *args, **kwargs):
     context = {
         'supervisor_configs': NodeManager().supervisor_configs,
@@ -16,6 +18,7 @@ def list_supervisor_configs_view(request, *args, **kwargs):
     return render(request, 'unicoremc/supervisor.html', context)
 
 
+@login_required(login_url='/')
 def list_nginx_configs_view(request, *args, **kwargs):
     context = {
         'nginx_configs': NodeManager().nginx_configs,
@@ -23,6 +26,7 @@ def list_nginx_configs_view(request, *args, **kwargs):
     return render(request, 'unicoremc/nginx.html', context)
 
 
+@login_required(login_url='/')
 def supervisor_config_view(request, *args, **kwargs):
     filename = request.GET.get('filename')
     if filename:
@@ -35,6 +39,7 @@ def supervisor_config_view(request, *args, **kwargs):
     return render(request, 'unicoremc/supervisor_detail.html', context)
 
 
+@login_required(login_url='/')
 def nginx_config_view(request, *args, **kwargs):
     filename = request.GET.get('filename')
     if filename:
@@ -47,6 +52,7 @@ def nginx_config_view(request, *args, **kwargs):
     return render(request, 'unicoremc/nginx_detail.html', context)
 
 
+@login_required(login_url='/')
 def new_project_view(request, *args, **kwargs):
     context = {
         'countries': constants.COUNTRIES
@@ -54,6 +60,7 @@ def new_project_view(request, *args, **kwargs):
     return render(request, 'unicoremc/new_project.html', context)
 
 
+@login_required(login_url='/')
 def test_ajax_function(request, *args, **kwargs):
     from time import sleep
     sleep(2)
