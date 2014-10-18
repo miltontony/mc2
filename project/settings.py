7a42e5a535192d94db9235ee2291d78b25eab4bd
Milton Madanda <madandat@gmail.com>
2014-10-18 13:02:41 +0200

    set login url

--- project/settings.py
+++ project/settings.py
@@ -133,7 +133,7 @@ AUTHENTICATION_BACKENDS = (
 )
 
 SOCIAL_AUTH_USER_MODEL = 'auth.User'
-LOGIN_URL = '/'
+LOGIN_URL = '/social/begin/github/'
 LOGIN_REDIRECT_URL = '/'
 
 ROOT_URLCONF = 'project.urls'
