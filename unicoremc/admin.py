a79f422991532cf559d394b608237953aa376bf6
Milton Madanda <madandat@gmail.com>
2014-10-24 16:47:54 +0200

    add admin for project

--- unicoremc/admin.py
+++ unicoremc/admin.py
@@ -7,6 +7,20 @@ from djcelery.models import (
 from django.contrib import admin
 from django.contrib.sites.models import Site
 
+from unicoremc.models import Project, Localisation
+
+
+class ProjectAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'app_type', 'country', 'state', 'base_repo_url', 'repo_url')
+    readonly_fields = (
+        'app_type', 'base_repo_url', 'country', 'state', 'repo_url', 'owner',
+        'available_languages')
+
+admin.site.register(Localisation, admin.ModelAdmin)
+admin.site.register(Project, ProjectAdmin)
+
 # remove celery from admin
 admin.site.unregister(Site)
 admin.site.unregister(TaskState)
