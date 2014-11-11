cdf1de616c5d5f53c6300c86dec3c58514109451
Milton Madanda <madandat@gmail.com>
2014-11-11 11:35:52 +0200

    ensure UC modules are updated on sideloader

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,3 +3,7 @@
 cp -a $REPO ./build/
 
 ${PIP} install -r $REPO/requirements.txt
+
+# ensure these dependencies are always updated in the sideloader workspace
+${PIP} uninstall elastic-git -y; ${PIP} install elastic-git
+${PIP} uninstall unicore-cms -y; ${PIP} install unicore-cms
