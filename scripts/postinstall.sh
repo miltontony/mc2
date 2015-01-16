a87f1540e21a1b44ead7736ca0c3dbf43a585af6
Milton Madanda <madandat@gmail.com>
2015-01-16 08:35:06 +0200

    restart mc processes in postinstall

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,3 +2,5 @@ manage="${VENV}/bin/python ${INSTALLDIR}/${REPO}/manage.py"
 
 $manage syncdb --noinput --no-initial-data --migrate
 $manage collectstatic --noinput
+
+sudo supervisorctl restart unicore_mc unicore_mc_celery
