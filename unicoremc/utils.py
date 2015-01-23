7e9839bbcb3e87177a9d21372714511c2d8a4003
Milton Madanda <madandat@gmail.com>
2015-01-23 13:50:35 +0200

    ensure default view is created

--- unicoremc/utils.py
+++ unicoremc/utils.py
@@ -28,4 +28,24 @@ def create_ga_profile(access_token, account_id, frontend_url, name):
             'name': name
         }
     ).execute()
+
+    profile_id = resp.get("id")
+    create_ga_default_view(access_token, account_id, profile_id)
+    return profile_id
+
+
+def create_ga_default_view(access_token, account_id, profile_id):
+    credentials = client.AccessTokenCredentials(
+        access_token,
+        'unicore-hub/1.0')
+    http = credentials.authorize(http=httplib2.Http())
+    service = discovery.build('analytics', 'v3', http=http)
+
+    resp = service.management().profiles().insert(
+        accountId=account_id,
+        webPropertyId=profile_id,
+        body={
+            'name': 'All Web Site Data'
+        }
+    ).execute()
     return resp.get("id")
