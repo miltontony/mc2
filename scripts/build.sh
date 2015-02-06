ea697505ac041fa07c9c34f25f823237caeecd64
Milton Madanda <madandat@gmail.com>
2015-02-06 11:06:38 +0200

    remove praekelt-pyramid-celery from workspace

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,9 @@
 
 cp -a $REPO ./build/
 
+# ensure `praekelt-pyramid-celery` is removed from sideloader workspace
+${PIP} uninstall praekelt-pyramid-celery -y
+
 ${PIP} install -r $REPO/requirements.txt -U
 
 # ensure these dependencies are always updated in the sideloader workspace
