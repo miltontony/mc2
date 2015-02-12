aed7b6046d17c0c87edd967c81a63cbc1f25b9d3
Milton Madanda <madandat@gmail.com>
2015-02-12 16:56:58 +0200

    fix pep8

--- unicoremc/forms.py
+++ unicoremc/forms.py
@@ -5,7 +5,9 @@ from django.forms import widgets
 
 class ProjectForm(forms.ModelForm):
     ga_profile_id = forms.CharField(required=False)
-    custom_dns = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'input-xxlarge'}))
+    custom_dns = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={'class': 'input-xxlarge'}))
     default_language = forms.ModelChoiceField(
         queryset=Localisation.objects.all(),
         empty_label="Unspecified",
