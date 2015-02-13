f4da0e84f850269774123646748303172456f8c0
Milton Madanda <madandat@gmail.com>
2015-02-13 11:08:53 +0200

    add test for nginx prod conf

--- unicoremc/tests/test_config_manager.py
+++ unicoremc/tests/test_config_manager.py
@@ -1,6 +1,7 @@
 import os
 
 from django.conf import settings
+from django.test.utils import override_settings
 
 from unicoremc.tests.base import UnicoremcTestCase
 
@@ -35,6 +36,23 @@ class ConfigManagerTestCase(UnicoremcTestCase):
         self.assertTrue(
             '/var/praekelt/unicore-cms-ffl/unicorecmsffl/static/' in data)
         self.assertTrue(frontend_socket_path in data)
+
+    @override_settings(DEPLOY_ENVIRONMENT='prod')
+    def test_write_frontend_nginx_configs_prod(self):
+        cm = self.get_config_manager()
+        cm.write_frontend_nginx('ffl', 'za', 'some.domain.com')
+
+        frontend_nginx_config_path = os.path.join(
+            settings.NGINX_CONFIGS_PATH,
+            'frontend_ffl_za.conf')
+
+        with open(frontend_nginx_config_path, "r") as config_file:
+            data = config_file.read()
+        self.assertTrue(
+            'server_name za.ffl.hub.unicore.io some.domain.com' in data)
+        self.assertTrue('za.ffl.hub.unicore.io' in data)
+
+        self.addCleanup(lambda: os.remove(frontend_nginx_config_path))
 
     def test_write_cms_nginx_configs(self):
         cm = self.get_config_manager()
