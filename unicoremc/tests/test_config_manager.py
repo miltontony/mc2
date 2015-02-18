a06aefc3883e20f0f04fabf818fa453159df6b08
Milton Madanda <madandat@gmail.com>
2015-02-18 15:00:44 +0200

    add cms custom domain

--- unicoremc/tests/test_config_manager.py
+++ unicoremc/tests/test_config_manager.py
@@ -56,7 +56,7 @@ class ConfigManagerTestCase(UnicoremcTestCase):
 
     def test_write_cms_nginx_configs(self):
         cm = self.get_config_manager()
-        cm.write_cms_nginx('ffl', 'za')
+        cm.write_cms_nginx('ffl', 'za', 'cms.domain.com')
 
         cms_nginx_config_path = os.path.join(
             settings.NGINX_CONFIGS_PATH,
@@ -74,6 +74,35 @@ class ConfigManagerTestCase(UnicoremcTestCase):
         self.addCleanup(lambda: os.remove(cms_nginx_config_path))
 
         self.assertTrue('cms.za.ffl.qa-hub.unicore.io' in data)
+        self.assertTrue(
+            'server_name cms.za.ffl.qa-hub.unicore.io cms.domain.com' in data)
+        self.assertTrue('unicore_cms_django_ffl_za-access.log' in data)
+        self.assertTrue('unicore_cms_django_ffl_za-error.log' in data)
+        self.assertTrue(cms_socket_path in data)
+
+    @override_settings(DEPLOY_ENVIRONMENT='prod')
+    def test_write_cms_nginx_configs_prod(self):
+        cm = self.get_config_manager()
+        cm.write_cms_nginx('ffl', 'za', 'cms.domain.com')
+
+        cms_nginx_config_path = os.path.join(
+            settings.NGINX_CONFIGS_PATH,
+            'cms_ffl_za.conf')
+
+        cms_socket_path = os.path.join(
+            settings.CMS_SOCKETS_PATH,
+            'ffl_za.socket')
+
+        self.assertTrue(os.path.exists(cms_nginx_config_path))
+
+        with open(cms_nginx_config_path, "r") as config_file:
+            data = config_file.read()
+
+        self.addCleanup(lambda: os.remove(cms_nginx_config_path))
+
+        self.assertTrue('cms.za.ffl.hub.unicore.io' in data)
+        self.assertTrue(
+            'server_name cms.za.ffl.hub.unicore.io cms.domain.com' in data)
         self.assertTrue('unicore_cms_django_ffl_za-access.log' in data)
         self.assertTrue('unicore_cms_django_ffl_za-error.log' in data)
         self.assertTrue(cms_socket_path in data)
