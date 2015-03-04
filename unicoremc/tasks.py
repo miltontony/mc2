31d3390c55cef5fd35bcd24ebdf7559b4b02a60d
Milton Madanda <madandat@gmail.com>
2015-03-04 09:52:36 +0200

    add more debug to push to git

--- unicoremc/tasks.py
+++ unicoremc/tasks.py
@@ -18,8 +18,10 @@ def start_new_project(project_id, access_token):
 def push_to_git(repo_path):
     workspace = EG.workspace(repo_path)
     if workspace.repo.remotes:
+        print 'pushing to git started'
         repo = workspace.repo
         remote = repo.remote()
         remote.fetch()
         remote_master = remote.refs.master
         remote.push(remote_master.remote_head)
+        print 'pushing to git finished'
