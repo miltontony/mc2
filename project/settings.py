9deb117cd03535f4a6780b48348a468a5aa7488a
Milton Madanda <madandat@gmail.com>
2015-07-09 15:31:41 +0200

    puppet templates still needed for cms

--- project/settings.py
+++ project/settings.py
@@ -148,6 +148,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    abspath('puppet_templates'),
     abspath('templates'),
 )
 
