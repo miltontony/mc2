b9a031eaed66f6ab6953c1ecba3f74838bdf898b
Rizmari Versfeld <rizziepit@gmail.com>
2015-07-28 22:14:11 +0200

    test org context processor

--- organizations/tests/test_utils.py
+++ organizations/tests/test_utils.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import AnonymousUser
 
+from mock import patch
+
+from organizations import context_processors
 from organizations.tests.base import OrganizationTestCase
 from organizations.utils import active_organization
 from organizations.models import (
@@ -24,3 +27,35 @@ class TestUtils(OrganizationTestCase):
         OrganizationUserRelation.objects.create(
             user=user, organization=organization)
         self.assertEqual(active_organization(request), organization)
+
+    def test_org_context_processor(self):
+        user = self.mk_user()
+        organization = self.mk_organization(users=[user])
+        request = self.mk_request('get', '/')
+        request.user = user
+        request.session[ORGANIZATION_SESSION_KEY] = organization.pk
+        context = context_processors.org(request)
+        self.assertEqual(context['active_organization'], organization)
+        self.assertEqual(set(context['organizations']), set([organization]))
+        self.assertTrue(context['is_active_organization_admin'])
+
+        with patch('organizations.context_processors.active_organization',
+                   return_value=None):
+            context = context_processors.org(request)
+            self.assertEqual(context['active_organization'], None)
+            self.assertEqual(
+                set(context['organizations']), set([organization]))
+            self.assertFalse(context['is_active_organization_admin'])
+
+        organization.organizationuserrelation_set.filter(
+            user=user).update(is_admin=False)
+        context = context_processors.org(request)
+        self.assertEqual(context['active_organization'], organization)
+        self.assertEqual(set(context['organizations']), set([organization]))
+        self.assertFalse(context['is_active_organization_admin'])
+
+        request.user = AnonymousUser()
+        context = context_processors.org(request)
+        self.assertEqual(context['active_organization'], None)
+        self.assertEqual(context['organizations'], [])
+        self.assertFalse(context['is_active_organization_admin'])
