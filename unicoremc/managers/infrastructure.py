78357379210a2c534bf9b94b0b2d2669041be0b3
Simon de Haan <simon@praekeltfoundation.org>
2015-08-17 15:24:25 +0200

    docstrings

--- unicoremc/managers/infrastructure.py
+++ unicoremc/managers/infrastructure.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.http import Http404
 
 import requests
 
@@ -16,29 +15,57 @@ class GeneralInfrastructureManager(object):
     }
 
     def get_marathon_app(self, app_id):
+        """
+        Returns the data dictionary for the given app_id
+
+        :param app_id str: The application id
+        :returns: dict
+        """
         return requests.get('%s/v2/apps/%s' % (
             settings.MESOS_MARATHON_HOST,
             app_id,
         ), headers=self.headers).json()['app']
 
     def get_marathon_app_tasks(self, app_id):
+        """
+        Returns the list of tasks for the given app_id
+
+        :param app_id str: The application id
+        :returns: list
+        """
         return requests.get('%s/v2/apps/%s/tasks' % (
             settings.MESOS_MARATHON_HOST,
             app_id,
         ), headers=self.headers).json()['tasks']
 
     def get_marathon_info(self):
+        """
+        Returns information on the Marathon host
+
+        :returns: dict
+        """
         return requests.get(
             '%s/v2/info' % (settings.MESOS_MARATHON_HOST,),
             headers=self.headers).json()
 
     def get_worker_info(self, hostname):
+        """
+        Returns info for the worker at the given hostname.
+
+        :returns: dict
+        """
         return requests.get(
             'http://%s:%s/state.json' % (hostname, settings.MESOS_HTTP_PORT),
             headers=self.headers
         ).json()
 
     def get_app_log_urls(self, app_id):
+        """
+        Returns a list of log URLs for logdriver for the given app_id
+
+        :param app_id str: The application id
+        :returns: list
+        """
         marathon_info = self.get_marathon_info()
         urls = []
         for task in self.get_marathon_app_tasks(app_id):
@@ -51,6 +78,16 @@ class GeneralInfrastructureManager(object):
 
     def get_task_log_urls(self, app_id, task_id, task_host,
                           marathon_info=None):
+        """
+        Returns a list of log URLs for logdriver for the given task_id
+
+        :param app_id str: the application id
+        :param task_id str: the task id
+        :param task_host str: the host where the task is running
+        :param marathon_info dict:
+            info dictionary returned from get_marathon_info, optional
+        :returns: generator
+        """
         marathon_info = marathon_info or self.get_marathon_info()
         framework_id = marathon_info['frameworkId']
         follower_id = self.get_worker_info(task_host)['id']
@@ -71,22 +108,49 @@ class GeneralInfrastructureManager(object):
 class ProjectInfrastructureManager(GeneralInfrastructureManager):
 
     def __init__(self, project):
+        """
+        A helper manager to get to a project's marathon app entries
+
+        :param project Project: A Project model instance
+        """
         self.project = project
 
     def get_project_marathon_app(self):
+        """
+        Returns the data dictionary for the current project's app_id.
+
+        :returns: dict
+        """
         return super(ProjectInfrastructureManager, self).get_marathon_app(
             self.project.app_id)
 
     def get_project_marathon_tasks(self):
+        """
+        Returns the task list for the current project's app_id
+
+        :returns: list
+        """
         return super(
             ProjectInfrastructureManager, self).get_marathon_app_tasks(
                 self.project.app_id)
 
     def get_project_log_urls(self):
+        """
+        Returns all the tasks log URLs for the current project's app_id
+
+        :returns: list
+        """
         return super(ProjectInfrastructureManager, self).get_app_log_urls(
             self.project.app_id)
 
     def get_project_task_log_urls(self, task_id):
+        """
+        Returns the log URLs for a given task for the current project's app_id.
+        Raises an InfrastructureError if task for task_id not found.
+
+        :param task_id str: the task id
+        :returns: list
+        """
         tasks = self.get_marathon_app_tasks(self.project.app_id)
         try:
             [task] = filter(lambda t: t['id'] == task_id, tasks)
