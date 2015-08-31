340d018eba9107fb6c734f3c04fbf9ce4043e50c
Milton Madanda <madandat@gmail.com>
2015-08-31 09:57:06 +0200

    add dependency to 0029 migration

--- unicoremc/migrations/0029_auto__add_field_project_organization.py
+++ unicoremc/migrations/0029_auto__add_field_project_organization.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("organizations", "0001_initial"),
+    )
+
     def forwards(self, orm):
         # Adding field 'Project.organization'
         db.add_column(u'unicoremc_project', 'organization',
