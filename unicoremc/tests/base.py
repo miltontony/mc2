9ca5801fe658fdc73f1bf993c65148cfe9966d3d
Milton Madanda <madandat@gmail.com>
2015-09-02 15:58:25 +0200

    add test

--- unicoremc/tests/base.py
+++ unicoremc/tests/base.py
@@ -216,6 +216,14 @@ class UnicoremcTestCase(TransactionTestCase, ModelBaseTest):
             content_type="application/json",
             status=status)
 
+    def mock_restart_marathon_app(self, project, status=200):
+        responses.add(
+            responses.POST, '%s/v2/apps/%s/restart' % (
+                settings.MESOS_MARATHON_HOST, project.app_id),
+            body=json.dumps({}),
+            content_type="application/json",
+            status=status)
+
     def mock_create_hub_app(self, **fields):
 
         def make_response(request):
