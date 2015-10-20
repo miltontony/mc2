34fb40783ddd666c14a62ee4b7f83857400275f2
Milton Madanda <madandat@gmail.com>
2015-10-20 13:21:45 +0200

    ensure new state is saved

--- unicoremc/views.py
+++ unicoremc/views.py
@@ -65,9 +65,11 @@ def update_marathon_exists_json(request, project_id):
 
     workflow = project.get_website_manager().workflow
     if project.state == 'done' and not project.exists_on_marathon():
-            workflow.take_action('missing')
+        workflow.take_action('missing')
+        project.save()
     elif project.state == 'missing' and project.exists_on_marathon():
         workflow.take_action('activate')
+        project.save()
 
     return HttpResponse(
         json.dumps({'state': project.state}), content_type='application/json')
