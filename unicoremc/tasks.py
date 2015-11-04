41a31406d3029041f6c4f402804641c5ad366d25
Milton Madanda <madandat@gmail.com>
2015-11-04 18:46:51 +0200

    remove unused celery task definition

--- unicoremc/tasks.py
+++ unicoremc/tasks.py
@@ -1,7 +1,5 @@
 from celery import task
 
-from elasticgit import EG
-
 
 @task(serializer='json')
 def start_new_controller(project_id):
@@ -9,14 +7,3 @@ def start_new_controller(project_id):
 
     controller = Controller.objects.get(pk=project_id)
     controller.get_builder().build()
-
-
-@task(serializer='json')
-def push_to_git(repo_path):
-    workspace = EG.workspace(repo_path)
-    if workspace.repo.remotes:
-        repo = workspace.repo
-        remote = repo.remote()
-        remote.fetch()
-        remote_master = remote.refs.master
-        remote.push(remote_master.remote_head)
