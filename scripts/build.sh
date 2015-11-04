f90a43dd7aae466fcc5e9b681c1acbb83b6db259
Milton Madanda <madandat@gmail.com>
2015-11-04 16:32:41 +0200

    update sideloader scripts for django 1.8

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,11 +2,4 @@
 
 cp -a $REPO ./build/
 
-# ensure `praekelt-pyramid-celery` is removed from sideloader workspace
-${PIP} uninstall praekelt-pyramid-celery -y
-
 ${PIP} install -r $REPO/requirements.txt -U
-
-# ensure these dependencies are always updated in the sideloader workspace
-# ${PIP} uninstall elastic-git -y; ${PIP} install elastic-git
-# ${PIP} uninstall unicore-cms -y; ${PIP} install unicore-cms
--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,6 +1,6 @@
 manage="${VENV}/bin/python ${INSTALLDIR}/${REPO}/manage.py"
 
-$manage syncdb --noinput --no-initial-data --migrate
+$manage migrate --noinput --no-initial-data
 $manage collectstatic --noinput
 
 sudo supervisorctl restart unicore_mc unicore_mc_celery
