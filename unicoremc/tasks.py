ce4dca572dd4dfb46e9ffa91d34673b7bd9ba344
Milton Madanda <madandat@gmail.com>
2015-11-07 09:05:35 +0200

    fix tests

--- unicoremc/tasks.py
+++ unicoremc/tasks.py
@@ -5,5 +5,5 @@ from celery import task
 def start_new_controller(project_id):
     from controllers.base.models import Controller
 
-    controller = Controller.objects.get(pk=project_id).as_leaf_class()
+    controller = Controller.objects.get(pk=project_id)
     controller.get_builder().build()
