7f322fb3c395c8a944274c1cbc8c1a6692f1e6c7
Milton Madanda <madandat@gmail.com>
2015-11-22 11:42:27 +0200

    update edit templates

--- controllers/docker/forms.py
+++ controllers/docker/forms.py
@@ -5,10 +5,12 @@ from controllers.base.forms import ControllerForm
 
 class DockerControllerForm(ControllerForm):
     docker_image = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'input-xxlarge'}))
+        widget=forms.TextInput(attrs={'class': 'form-control'}))
     marathon_health_check_path = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'input-medium'}),
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
         required=False)
+    port = forms.CharField(
+        widget=forms.NumberInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = DockerController
