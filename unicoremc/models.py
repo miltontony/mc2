1f9946fdc6cddaf731fcce4e620640fb76048eeb
Milton Madanda <madandat@gmail.com>
2015-11-25 09:16:37 +0200

    add user settings model

--- unicoremc/models.py
+++ unicoremc/models.py
@@ -1,1 +1,19 @@
-# models go here
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class UserSettings(models.Model):
+    BASIC_SETTINGS_LEVEL = 'basic'
+    ADVANCED_SETTINGS_LEVEL = 'advanced'
+    EXPERT_SETTINGS_LEVEL = 'expert'
+    SETTINGS_LEVEL_CHOICES = (
+        (BASIC_SETTINGS_LEVEL, 'Basic'),
+        (ADVANCED_SETTINGS_LEVEL, 'Advanced'),
+        (EXPERT_SETTINGS_LEVEL, 'Expert'),
+    )
+    user = models.OneToOneField(
+        User, related_name="settings", primary_key=True)
+    settings_level = models.CharField(
+        max_length=100,
+        choices=SETTINGS_LEVEL_CHOICES,
+        default=BASIC_SETTINGS_LEVEL)
