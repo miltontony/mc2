7ccd3505dd81e5acf013f1d9863f9124b34eba64
Milton Madanda <madandat@gmail.com>
2015-12-11 15:41:02 +0200

    redo templates layout

--- project/settings.py
+++ project/settings.py
@@ -95,8 +95,8 @@ SECRET_KEY = 'please-change-me'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
+    'django.template.loaders.filesystem.Loader',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
