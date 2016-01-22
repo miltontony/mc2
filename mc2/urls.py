f17cbd9fa53569e5c10b0f48b856449b1da5afd5
Milton Madanda <madandat@gmail.com>
2016-01-22 09:20:35 +0200

    use stock django urls

--- mc2/urls.py
+++ mc2/urls.py
@@ -14,16 +14,6 @@ urlpatterns = patterns(
         views.HomepageView.as_view(),
         name='home'
     ),
-    url(
-        r'^accounts/password/reset/$', password_reset,
-        {'template_name': 'password_reset_form.html'},
-        name='password_change'),
-    url(
-        r'^accounts/password/reset/sent/$',
-        'django.contrib.auth.views.password_reset_done',
-        {'template_name': 'password_reset_done.html'},
-        name='password_reset_done'),
-
     url(r'^login/?$', views.MC2LoginView.as_view(), name='login'),
     url(r'', include('mama_cas.urls')),
     url(
@@ -31,6 +21,7 @@ urlpatterns = patterns(
         'django.contrib.auth.views.logout_then_login',
         name='logout'
     ),
+    url('^', include('django.contrib.auth.urls')),
     url(
         r'^settings/update/$',
         login_required(views.UserSettingsView.as_view()),
