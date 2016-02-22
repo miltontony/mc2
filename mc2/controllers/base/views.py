245c4db1d1cc921c63fbd9e6677e06134cf74257
Milton Madanda <madandat@gmail.com>
2016-02-22 14:29:43 +0200

    move form_valid to docker controller view

--- mc2/controllers/base/views.py
+++ mc2/controllers/base/views.py
@@ -88,7 +88,6 @@ class ControllerCreateView(ControllerViewMixin, CreateView):
         form.controller_form.instance.save()
 
         form.env_formset.instance = form.controller_form.instance
-        form.labels_formset.instance = form.controller_form.instance
 
         response = super(ControllerCreateView, self).form_valid(form)
         tasks.start_new_controller.delay(form.controller_form.instance.id)
