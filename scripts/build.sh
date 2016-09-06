974f42fb65a1430bbbc610aff0b1c8c244f5807a
Milton Madanda <madandat@gmail.com>
2016-09-06 10:31:35 +0200

    ensure sideloader installs django first

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,8 @@ cp -a $REPO ./build/
 # ensure `praekelt-pyramid-celery` is removed from sideloader workspace
 ${PIP} uninstall praekelt-pyramid-celery -y
 
+# install Django first - workaround for django-appregister: it tries to install the latest django
+${PIP} install "Django<1.7"
 ${PIP} install -r $REPO/requirements.txt -U
 
 # ensure these dependencies are always updated in the sideloader workspace
